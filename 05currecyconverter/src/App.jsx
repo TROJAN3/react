9ed63cebb39d12a8f92f89023d06e405aed557df
Deepak Bharadwaj <deepakbharadwaj@Deepaks-MacBook-Air.local>
2025-03-31 23:18:34 +0530

    implemented currency converter

---
+++ 05currecyconverter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' 
+   style={{backgroundImage: `url('https://images.pexels.com/photos/106152/euro-coins-currency-money-106152.jpeg?auto=compress&cs=tinysrgb&w=600')`,backgroundSize: 'cover'}}>
+   
+   
+   
+   </div>
+  )
+}
+
+export default App
---
+++ 05currecyconverter/src/components/inputBox.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function inputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions=[],
+    selectedCurrency='USD',
+    amountDisabled=false,
+    currencyDisabled=false,
+    className='',
+}) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className='w-1-2'>
+            <label className='text-black/40 mb-2 inline-block'>{label}</label>
+            <input type="number"
+            className='outline-none w-full bg-transparent py-1.5'
+            placeholder='Amount'
+            disabled={amountDisabled}
+            value={amount}
+            onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+            />
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default inputBox
